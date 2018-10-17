93cf8202625433c43c5693cb8b6ecfb13590ac29
정형섭 <sliverjung@naver.com>
2018-10-17 23:03:08 +0900

    학식 글씨 깨지는지 확인

--- src/food.js
+++ src/food.js
@@ -1,38 +1,29 @@
-const request = require('request');
-const cheerio = require('cheerio');
-var Iconv = require('iconv').Iconv;
+var cheerio = require('cheerio');
+var request = require('request');
 
-const url = "http://kmucoop.kookmin.ac.kr/restaurant/restaurant.php?w=1";
+var url = "http://kmucoop.kookmin.ac.kr/restaurant/restaurant.php?w=1";
 
-request(url, (error, response, body) => {
+request(url, function(error, response, html){
   if (error) throw error;
 
-//    var strContents = new Buffer(body, 'binary');
-//    var iconv = new Iconv('EUC-KR', 'UTF-8//TRANSLIT');
-//    var strContents8 = iconv.convert(strContents).toString();
-//    var $ = cheerio.load(strContents8);
+  var $ = cheerio.load(html);
 
-    var $ = cheerio.load(body);
-    console.log($('body > div > div > div.dv1 > div.smid > div.detail > table:nth-child(3) > tbody').text().trim());
+  try{
+      $('table.ft1').find('tr').find('td').each(function(index, elem){
+
+              //if(index==3) {
+                  var date = $(this).text();
+
+
+                  console.log(date);
+              //}
+
+        //console.log($(this).text());
+      });
+  }catch (error) {
+      console.log(error);
+  }
+
 
 });
-/*
-var strContents = new Buffer(body, 'binary');
-    var iconv = new Iconv('euc-kr', 'utf-8');
-    strContents = iconv.convert(strConTents).toString('utf-8');
-    var $ = cheerio.load(strContents);
 
-
-$('table').find('tr').each(function (index, elem) {
-      if (index % 6 === 0) {
-        krDay = $(this).find('th').text().trim();
-
-        console.log(`${krDay}`);
-      } else {
-        corner = $(this).find('th').text().trim();
-        menu = $(this).find('th').next().text().trim();
-
-        console.log(`${corner} -> ${menu}`);
-      }
-    });
-    */
