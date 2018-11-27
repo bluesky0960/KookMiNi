f5b71174242a7d0cb7997d5275c2d027a9330d7f
bluesky0960 <bluesky096049@gmail.com>
2018-11-27 17:13:35 +0900

    그냥 일단 푸시해요

--- kookminijs_chat/src/App.js
+++ kookminijs_chat/src/App.js
@@ -3,6 +3,7 @@ import firebase, {auth, provider}from './firebase';
 import {connect} from 'react-redux'
 import './App.css';
 import {sendMessage} from'./chat';
+import {library} from './library_place';
 
 class App extends Component {
     constructor(props){
@@ -160,6 +161,7 @@ class App extends Component {
                                             else {
                                                 this.getMemoList();                                            
                                                 this.keyReset();
+                                                library();
                                             }
                                         }}>리스트</button>
                                     <button onClick={()=>{
@@ -169,7 +171,7 @@ class App extends Component {
                                         else {
                                             sendMessage(this.state.input);
                                             this.keyReset();
-                                            this.state.input='';
+                                            //this.input.setState('');
                                         }
                                     }}>입력</button>
                                     <button onClick={() => {
